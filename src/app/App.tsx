44cb2f5419d6fa6919d8015492fb02d289ac0114
VictoriaOleinyk <vikacrimea@yandex.ru>
2025-06-19 17:37:03 +0300

    creating application architecture

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useEffect, useState } from 'react'
+import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
 	const [count, setCount] = useState(0)
-
+	useEffect(() => {
+		console.log(count)
+	}, [])
 	return (
 		<>
 			<div>
+				<h1>Welcome</h1>
 				<a href="https://vite.dev" target="_blank">
 					<img src={viteLogo} className="logo" alt="Vite logo" />
 				</a>
